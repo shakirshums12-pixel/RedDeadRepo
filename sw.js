676a5319f5da7d47d1f9d7fda64c1812c0c4808b
shakirshums12-pixel <shakirshums12@gmail.com>
2025-08-20 09:59:01 +0530

    second commit

---
+++ sw.js
@@ -0,0 +1,46 @@
+// sw.js
+
+const CACHE_NAME = "RDR-v1";
+const urlsToCache = [
+  "/",
+  "./index.html",
+  "./styles/style.css",
+  "./scripts.js",
+  "/favicons/android-icon-192x192.png",
+  '/favicons/manifest.json'
+];
+
+// Install Service Worker and cache files
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Activate Service Worker (clean old caches if any)
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            console.log("Deleting old cache:", cache);
+            return caches.delete(cache);
+          }
+        })
+      )
+    )
+  );
+});
+
+// Fetch requests and serve cached files when offline
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
